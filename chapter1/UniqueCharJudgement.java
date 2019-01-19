f055aee47da06ac60e436590c0c053e958934897
Akihiro Yamada <u.parr917@gmail.com>
2019-01-19 21:04:25 +0900

    重複した文字列があるか判定する処理をメソッド化。

--- chapter1/UniqueCharJudgement.java
+++ chapter1/UniqueCharJudgement.java
@@ -12,6 +12,29 @@ class UniqueCharJudgement {
         String str = scanner.nextLine();
 
         try {
+            //入力した文字列内に重複した文字があるか否かで分岐。
+            if (judgeDuplicateCharExistance(str)) {
+                //重複した文字がある場合。
+                System.out.println("重複した文字が存在します。");
+            } else {
+                System.out.println("全て固有の文字です。");
+            }
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            } 
+        }
+        
+    }
+
+    /**
+     * 文字列に重複した文字が存在するか否かを判定。
+     * @param str 判定対象の文字列。
+     * @return 重複した文字が存在する->true。
+     *         重複した文字が存在しない->false。
+     */
+    private static boolean judgeDuplicateCharExistance(String str) {
+
             //一文字ずつ重複した文字がないか順番に調べる。
             //最後から2番目の文字を調べて、重複した文字がなければ
             //文字列に重複した文字はないことが確定するため、
@@ -22,18 +45,12 @@ class UniqueCharJudgement {
 
                 for (int j = i + 1; j < str.length(); j++) {
                     if (targetChar == str.charAt(j)) {
-                        System.out.println("重複した文字が存在します。");
-                        return;
+                        return true;
                     }
                 }
             }
-            System.out.println("全て固有の文字です。");
+            return false;
 
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
-        }
     }
 
 }
